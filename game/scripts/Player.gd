838c789ae81ffcb1c55b01daf4f80c2063e1c19e
nalquas <nalquas.dev@gmail.com>
2020-09-05 13:52:25 +0200

    Player is now an orbital laser beam

--- game/scripts/Player.gd
+++ game/scripts/Player.gd
@@ -1,8 +1,18 @@
 extends KinematicBody2D
 
-export (float) var speed = 250.0
+export (float) var speed = 150.0
+
+func _ready():
+	# Make the laser circle appear below all other objects
+	$AnimatedSprite_Circle.z_as_relative = false
+	$AnimatedSprite_Circle.z_index = -9223372036854775808 #-2^63
+	
+	# Start animations
+	$AnimatedSprite_Laser.play()
+	$AnimatedSprite_Circle.play()
 
 func _physics_process(_delta):
+	# Handle movement
 	var direction = Vector2(0, 0)
 	if Input.is_action_pressed("up"):
 		direction.y -= 1
