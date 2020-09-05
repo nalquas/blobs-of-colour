49012fe81df1d3dbb97804743ada1e792a3adfa5
nalquas <nalquas.dev@gmail.com>
2020-09-05 23:43:37 +0200

    Show colour spread in the pie chart

--- game/scripts/Game.gd
+++ game/scripts/Game.gd
@@ -13,6 +13,28 @@ func _process(_delta):
 	# Make objects of the group "order" appear in proper draw order
 	for object in get_tree().get_nodes_in_group("order"):
 		object.z_index = object.position.y
+	
+	# Handle scoring
+	var total = float(80*45)
+	
+	var green = 0
+	var blue = 0
+	var orange = 0
+	var pink = 0
+	for x in range(0, 80):
+		for y in range(0, 45):
+			var index = $PaintMap.get_cell(x, y)
+			match index:
+				0:
+					green += 1
+				1:
+					blue += 1
+				2:
+					orange += 1
+				3:
+					pink += 1
+	$GameOverlay/PieChart.setColours(green / total, blue / total, orange / total, pink / total)
+	$GameOverlay/PieChart.update()
 
 func _physics_process(_delta):
 	# Handle blobs painting the world (the tilemap "PaintMap")
